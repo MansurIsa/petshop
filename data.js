6022935445d572359233cde2cfeb085acd04191d
MansurIsa <mansur69722@gmail.com>
2024-06-17 23:00:19 +0400

    initialize

---
+++ data.js
@@ -0,0 +1,91 @@
+let products = [
+    {
+        id: 1,
+        img: "./images/closeup-shot-ginger-cat-wicker-basket-isolated-white-wall_1-removebg-preview.png",
+        name: "Pişik evi",
+        price: 42
+    },
+    {
+        id: 2,
+        img: "./images/street-fashioned-brown-white-dog-cool-black-hoodie-trucker-cap-with-mesh-back-rustic-wooden-table 1.png",
+        name: "İt üçün köynək",
+        price: 60
+    },
+    {
+        id: 3,
+        img: "./images/pets-food-wooden-floor 1.png",
+        name: "Yemək qabları",
+        price: 35
+    },
+    {
+        id: 4,
+        img: "./images/image 1 (1).png",
+        name: "Müxtəlif tasmalar",
+        price: 22
+    }
+]
+
+
+let products2 = [
+    {
+        id: 5,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 45,
+        x: "enCoxSatilan"
+    },
+    {
+        id: 6,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 9,
+        x: "secilenMehsullar"
+    },
+    {
+        id: 7,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 6,
+        x: "yeniMehsullar"
+    },
+    {
+        id: 8,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 89,
+        x: "enCoxSatilan"
+    },
+    {
+        id: 9,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 25,
+        x: "secilenMehsullar"
+    },
+    {
+        id: 10,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 15,
+        x: "enCoxSatilan"
+    },
+    {
+        id: 11,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 5,
+        x: "yeniMehsullar"
+    },
+    {
+        id: 12,
+        img: "./images/Me-O Cat Food - Pets - Package Inspiration 1.png",
+        name: "Pişiklər üçün quru və nəm lasos",
+        price: 40,
+        x: "secilenMehsullar"
+    }
+]
+
+
+let basket = JSON.parse(localStorage.getItem("sebet")) || [
+
+]
